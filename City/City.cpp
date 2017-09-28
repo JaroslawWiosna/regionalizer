f07c3e931718b65a6c72e53fbf2c0eed72aa2d63
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-09-28 20:26:44 +0200

    New class "City" introduced

---
+++ City/City.cpp
@@ -0,0 +1,63 @@
+/**
+ * \class City
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include "City.hpp"
+
+City::City(std::string name,
+        std::size_t area,
+        std::size_t population,
+        std::string latitude,
+        std::string longitude) : 
+            name{name},
+	    area{area},
+	    population{population},
+	    latitude{latitude},
+	    longitude{longitude} {
+
+}
+    
+double City::distanceFrom(City differentCity) {
+   // below we have Coordinates as decimal values
+   // It means that 21E30' would be 21.5
+   //               21E45' would be 21.75, and so on...
+   // TODO: move this explanation into doxygen comment
+
+   // A = this city
+   // B = differentCity
+
+   // TODO: convert this->latitude to Alat
+   // TODO: convert this->longitude to Alon
+   // TODO: convert differentCity.latitude to Blat
+   // TODO: convert differentCity.latitude to Blon
+    double Alat = 51.75;
+    double Alon = 19.49; //19deg 27' 17''
+    double Blat = 51.24; // 51deg 13' 56''
+    double Blon = 21.01; //21deg 00' 30''
+
+    constexpr double R = 6371;
+    // TODO: implement generic function for radian conversion
+    // like Alat = utils::degreesToRadians(Alat);
+    // (consider creating utils directory with namespace 'utils'
+    Alat = Alat * 3.14 / 180.0; // to_radians
+    Alon = Alon * 3.14 / 180.0; // to_radians
+    Blat = Blat * 3.14 / 180.0; // to_radians
+    Blon = Blon * 3.14 / 180.0; // to_radians
+    double deltaLat = Blat - Alat;
+    double deltaLon = Blon - Alon;
+
+    double a = pow(sin(0.5 * deltaLat),2) + cos(Alat) * cos(Blat) * pow(sin(0.5 * deltaLon),2);
+    double c = 2 * atan2(sqrt(a),sqrt(1-a));
+    double d = R * c;
+
+    return d;
+}
+ 
+
+
---
+++ City/City.hpp
@@ -0,0 +1,53 @@
+/**
+ * \class City
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef CITY_HPP
+#define CITY_HPP
+
+#include <iostream>
+//#include <list>
+//#include <cfloat> // FLT_MIN
+#include <cmath> // abs
+//#include <algorithm>
+//#include <vector>
+#include <string>
+//#include <memory>
+//#include <utility>
+//#include <type_traits>
+
+class City{
+  public: 
+    explicit City() : 
+        name{""}, 
+        area{0},
+	population{0},
+	latitude{"30N40'40''"},
+	longitude{"15E0'0''"} {
+
+    }
+    
+    City(std::string name,
+        std::size_t area,
+        std::size_t population,
+        std::string latitude,
+        std::string longitude
+    );
+    
+    double distanceFrom(City differentCity);
+  private:
+    std::string name;
+    std::size_t area;
+    std::size_t population;
+    std::string latitude;
+    std::string longitude;
+};
+
+#endif // CITY_HPP
+
