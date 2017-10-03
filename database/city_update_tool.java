fa35f2ea01b011aef31471d16d69e9a09508210b
Wojciech Mielczarek <kermit10000000@gmail.com>
2017-10-03 22:10:38 +0100

    Fixed version of java files for making city database with the database itself.

--- database/city_update_tool.java
+++ database/city_update_tool.java
@@ -1,9 +1,20 @@
 package cities_updater_tool;
 
-/*
+/**
+ * @brief creates database of cities in poland with their name, area and population number
+ * 
+ * @condition_on_start webpage we use https://pl.wikipedia.org/wiki/Miasta_w_Polsce_(statystyki) must exist 
+ * 					   and contains useful for this method informations
+ * 
+ * @way_of_working Open the webpage and adds the cities from the webpage to the file.  		
+ * 				   It does that by cutting the needed values from the web html.
  * @authors Wojciech Mielczarek
  * @authors Jaroslaw Wiosna
- * gets cities list with name, population, area
+ * 
+ * @todo TODOs are in city_updater_tool todo
+ * 
+ * @condition_on_stop creates final file cities_data_pre_parsed.txt with all cities that he founds the coordinates for, 
+ * 					  on the given webpage
  */
 
 import java.io.FileNotFoundException;
@@ -15,52 +26,60 @@ import java.net.URLConnection;
 
 // not working so far
 public class city_update_tool {
-    public static void main(String[] args) {
-        try {
-            URL data = new URL("https://pl.wikipedia.org/wiki/Miasta_w_Polsce_(statystyki)");
-            URLConnection connection = data.openConnection();
-            BufferedReader web_data = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-            int linecount=0;
-            PrintWriter zapis = new PrintWriter("cities_data.txt");
-            boolean start_writing=false;
-            while ((inputLine = web_data.readLine()) != null) {  
-            	if(start_writing)linecount++;
-            	if(linecount>1&&linecount<4)continue;
-            	if(start_writing)System.out.println(inputLine);
-                if(inputLine.contains("Edytuj sekcj"))start_writing=false;
-                if(start_writing&&(inputLine.contains("title=")||inputLine.contains("tabela-liczba"))){
-					switch(linecount){
-						case 1:
-	                	String line[] = inputLine.split(">");
-						String line2[] = line[1].split("<");
-	                	zapis.write(line2[0]+" ");
-	                	break;
-						case 4:
-	                	String line3[] = inputLine.split(">");
-						String line4[] = line3[1].split("<");
-	                	zapis.write(line4[0]+" ");
-	                	break;
-						case 5:
-						String line5[] = inputLine.split(">");
-						String line6[] = line5[1].split("<");
-		                zapis.write(line6[0]+" ");
-		                break;
-						case 6:
-						String line7[] = inputLine.split(">");
-						String line8[] = line7[1].split("<");
-		                zapis.write(line8[0]+" ");
-						zapis.println(" ");
-		                break;
+	public static void main(String[] args) {
+		String city_argument_splitter="|";
+		try {
+			URL data = new URL("https://pl.wikipedia.org/wiki/Miasta_w_Polsce_(statystyki)");
+			URLConnection connection = data.openConnection();
+			BufferedReader web_data = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+			String inputLine;
+			int linecount=0;
+			PrintWriter zapis = new PrintWriter("cities_data_pre_parsed.txt");
+			boolean start_writing=false;
+			String writeToFile="";
+			while ((inputLine = web_data.readLine()) != null) { 
+				if(inputLine.contains("Liczba ludno")||inputLine.contains("<h2><span id="))start_writing=false;      
+				if(inputLine.contains("<b>")&&inputLine.contains("</a></b></td>")){start_writing=true;linecount=0;} 
+				if(start_writing){
+					linecount++;
+					if(linecount==2||linecount==3)continue;
+				}           
+					if(start_writing&&(inputLine.contains("title=")||inputLine.contains("tabela-liczba"))){
+					String list[]=inputLine.split(">");
+					if(linecount==1){
+						if(inputLine.contains("<b>")&&inputLine.contains("<i>")){String list2[]=list[4].split("<"); writeToFile+=list2[0]+city_argument_splitter;}
+						else if(inputLine.contains("<b>")){String list2[]=list[3].split("<"); writeToFile+=list2[0]+city_argument_splitter;}
+						else {String list2[]=list[2].split("<"); writeToFile+=list2[0]+city_argument_splitter;}
+						}
+					if(linecount==4||linecount==5){ 
+						String list2[]=list[1].split("<"); 
+						if(linecount==4)
+							writeToFile+=(list2[0]+city_argument_splitter);
+						else if(linecount==5){
+							String list3[]=list2[0].split("&");
+							String list4[]=list2[0].split(";");
+							if(list2[0].contains(";")){
+								String list5[]=list3[1].split(";");
+								Integer liczbaludnosc=0;
+								if(list4.length==2)liczbaludnosc = Integer.parseInt(list3[0])*1000+
+														Integer.parseInt(list4[1]);
+								if(list4.length==3)liczbaludnosc = Integer.parseInt(list3[0])*1000000+
+										1000*Integer.parseInt(list5[1])+Integer.parseInt(list4[2]);
+								writeToFile+=(liczbaludnosc.toString());
+								} 
+							else writeToFile+=(list2[0]);
+							zapis.println(writeToFile);
+							writeToFile="";}
 					}
-                }
-                if(linecount==6)linecount=0;
-                if(inputLine.contains("zaludnienia"))start_writing=true;
-            }
-            zapis.close();
-            web_data.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+				}
+				if(linecount==8){linecount=0;
+				}
+
+			}
+			zapis.close();
+			web_data.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
