9d91433f029573d628453c4675441d0162a75f3d
Wojciech Mielczarek <kermit10000000@gmail.com>
2017-10-12 19:49:42 +0100

    it will prevent from further bugs in case of additional whitespaces in the database Closes #18
    Signed-off-by: Wojciech Mielczarek <kermit10000000@gmail.com>

--- DatabaseReader/DatabaseReader.cpp
+++ DatabaseReader/DatabaseReader.cpp
@@ -21,10 +21,14 @@ std::vector<City> readCitiesFromFile(std::string databaseFile) {
             continue;
 	}
         std::vector<std::string> params;
-	split(line, "|", params);
+        size_t pos;
+        while((pos = line.find(" "))!=std::string::npos) {
+            line.replace(line.begin()+pos,line.begin()+pos+1,"");
+        }
+	    split(line, "|", params);
 
         City city(params[0], params[1], params[2], params[3], params[4]);
-	vecOfCities.push_back(city);
+	    vecOfCities.push_back(city);
     }
 
     return vecOfCities;
