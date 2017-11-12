78a7d4e8a7f01e2348db85bf24635af9a38a9d7e
JaroslawWiosna <JaroslawWiosna@users.noreply.github.com>
2017-11-12 16:29:07 +0100

    Comments for unittest in Region Class (#111)

--- tests/region/region.cpp
+++ tests/region/region.cpp
@@ -14,21 +14,31 @@
 #include <cmath>
 #include <gtest/gtest.h>
 
-TEST(Region, dummyTest) {
+/**
+ * This test should cover operator+= and operator+ as well
+ */
+TEST(Region, OperatorPlusTest) {
     Region region{};
 
-    City a{"Aleksandrow Kujawski","7.23","12335","52.8833","18.7"};
-City b{"Aleksandrow Lodzki","13.82","21380","51.8167","19.3167"};
-City c{"Alwernia","8.88","3402","50.0667","19.5333"};
+    City a{"Aleksandrow Kujawski", "7.23", "12335", "52.8833", "18.7"};
+    City b{"Aleksandrow Lodzki", "13.82", "21380", "51.8167", "19.3167"};
+    City c{"Alwernia", "8.88", "3402", "50.0667", "19.5333"};
 
     region += a;
-    ASSERT_EQ(region.getPopulation(), a.getPopulation());
+    ASSERT_EQ(region.getPopulation(), a.getPopulation())
+        << "Population of region consists of one city should be equal to the "
+           "population of this city.";
 
     region += b;
-    ASSERT_EQ(region.getPopulation(), a.getPopulation() + b.getPopulation());
+    ASSERT_EQ(region.getPopulation(), a.getPopulation() + b.getPopulation())
+        << "Population of region consists of two cities should be equal to the "
+           "sum of population of those cities";
 
     region += c;
-    ASSERT_EQ(region.getPopulation(), a.getPopulation() + b.getPopulation() + c.getPopulation());
+    ASSERT_EQ(region.getPopulation(),
+              a.getPopulation() + b.getPopulation() + c.getPopulation())
+        << "Population of region consists of three cities should be equal to "
+           "the sum of population of those cities";
 }
 
 int main(int argc, char *argv[]) {
