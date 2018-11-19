f261eee1e9b125c4dc173cc0c93d150f58807b15
Jarosław Wiosna <179892@edu.p.lodz.pl>
2018-11-19 20:57:24 +0100

    Initialize polish i18n
    i18n stands for `internationalization`

--- lang.sh
+++ lang.sh
@@ -3,7 +3,7 @@
 #msginit --input=po/regionalizer.pot --locale=pl --output=po/pl/regionalizer.po
 #msgfmt --output-file=po/pl/regionalizer.mo po/pl/regionalizer.po
 
-xgettext --keyword=_ --language=C --add-comments --sort-output -o po/regionalizer.pot main.cpp
+xgettext --keyword=_ --language=C --add-comments --sort-output -o po/regionalizer.pot src/main.cpp
 msgmerge --update po/pl/regionalizer.po po/regionalizer.pot
 msgfmt --output-file=po/pl/regionalizer.mo po/pl/regionalizer.po
 
